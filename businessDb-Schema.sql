fbfe01d982b4cf581a0627524a78f36008f95d06
Vinette Gutierrez <vinette.gutierrez@gmail.com>
2021-06-13 14:00:57 -0700

    Added files and created original schema

---
+++ businessDb-Schema.sql
@@ -0,0 +1,26 @@
+CREATE database businessDB;
+
+USE businessDB;
+
+CREATE TABLE department (
+id INT NOT NULL AUTO_INCREMENT,
+name varchar(30),
+PRIMARY KEY (id)
+);
+
+CREATE TABLE postiion (
+id INT NOT NULL AUTO_INCREMENT,
+title varchar(30),
+salary dec(10,2), 
+department_id INT NOT NULL, 
+PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+id INT NOT NULL AUTO_INCREMENT,
+first_name varchar(30),
+last_name varchar(30),
+role_id INT,
+manager_id INT,
+PRIMARY KEY (id)
+);
